0d1f7aa99479d2bbbce8b017eb001cc6b53337c6
Bernava <bernava@hugtak.com>
2023-02-28 15:53:37 -0300

    add todo's

---
+++ lib/modules/bug_detailed/bug_detailed_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class BugDetailedModule extends Module {
+  //TODO desenvolver BUG MODULE.
+  @override
+  List<Bind<Object>> get binds => [];
+  @override
+  List<ModularRoute> get routes => [];
+}
